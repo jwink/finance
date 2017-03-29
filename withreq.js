133e34b0e41fd4145cd0617a183dba528338b45a
jwink <jeff_winkler@yahoo.com>
2017-03-29 14:35:40 -0400

    stuff dos

--- withreq.js
+++ withreq.js
@@ -10,14 +10,18 @@ var baseURL = 'https://query.yahooapis.com/v1/public/yql?q=';
 var histPricesSection = 'select * from yahoo.finance.historicaldata where ';
 //var symbolInfo = 'symbol%20%3D%20%22^VIX%22%20and%20';
 
-var currSymbol = '^VIX';
+var realTimePx = 'select * from yahoo.finance.quotes where ';
+
+var currSymbol = 'GRPN';
 var currStartDate = '2017-03-01';
 var currEnddate = '2018-01-01';
+//var currStartDate = '2017-03-28';
 
-var symbolInfo = 'symbol = "' + currSymbol + '" and ';
+var symbolInfo = 'symbol = "' + currSymbol + '"';
+
 
 //var startDate = 'startDate%20%3D%20%222016-11-30%22%20and%20';
-var startDate = 'startDate = "' + currStartDate + '" and ';
+var startDate = ' and startDate = "' + currStartDate + '" and ';
 
 var endDate = 'endDate = "' + currEnddate +  '"';
 
@@ -34,4 +38,18 @@ request(baseURL
   console.log(data.query.results.quote);
 });
 
+
+var what = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22YHOO%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback='
+
+var whatDos = baseURL + escape(realTimePx) + escape(symbolInfo) + endURLOptions;
+
+
+console.log(unescape(what));
+
+request(whatDos, function(error, response, body) {
+  console.log('error:', error); // Print the error if one occurred 
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+  var data = JSON.parse(body);
+  console.log(data.query.results.quote);
+});
 
